8954b4ad55f18fba9f9d3425c2ccee2b6b499002
Adam Robertson <oldskool73@gmail.com>
2016-03-23 11:27:40 +1100

    Full rewrite - basic open/close logic in place

--- autotop986.ino
+++ autotop986.ino
@@ -1,29 +1,29 @@
 /*
-  986 Auto-Top by Alex Roy
+  986 Auto-Top by Adam Robertson <oldskool73@gmail.com>
+
+  Based on : 986 Auto-Top by Alex Roy
+
   This auto-presses the open or close buttons for the convertible top on a Porsche 986
   for the specified open/close seconds automatically if you quickly tap the button.
-  If the button tap is longer (eg it is held down), the button is only auto-pressed
-  for the same amount of time as the user is pressing.
+
+  If the button tap is longer (eg it is held down), the button reacts like normal
+  and is only auto-pressed for the same amount of time as the user is pressing.
 
   If the top is already automatically opening or closing, a second press of either
   button cancels the auto-open proceedure.
 
-  My 2001 Boxster S open/close timing:
-  13.6sec to open
-  14.4sec to close
 */
 
+#include "Switch.h"
+#include "SimpleTimer.h"
 
 // pin definitions
-#define openBtnOut     2
-#define closeBtnOut    3
-#define pBrakeOut      4
-#define openBtnIn      8
-#define closeBtnIn     9
+#define pinOpenBtnOut       2
+#define pinCloseBtnOut      3
+#define pinParkingBrakeOut  4
+#define pinOpenBtnIn        8
+#define pinCloseBtnIn       9
 
-// cutoff point, after which a press is considered a long press
-#define shortPressInterval 250
-// loop delay
 #define loopInterval 50
 
 // time taken to open top
@@ -32,171 +32,210 @@
 #define closeTime 16400
 
 // operation modes
-#define modeNull 0
+#define modeWait 0
 #define modeAutoOpen 1
 #define modeAutoClose 2
 #define modeManualOpen 3
 #define modeManualClose 4
+#define modeHalt 5
 
-int closeBtnCount = 0;
-int openBtnCount = 0;
-int movementCount = 0;
+unsigned long ms, motorStartTime;
 
-int openMode = modeNull;
-bool btnReleased = false;
-bool ignoreBtn = false;
+int currentMode = modeWait;
+int previousMode = modeWait;
 
+String lastLog = "null";
+
+Switch openButtonIn = Switch(pinOpenBtnIn);//,INPUT, HIGH);
+Switch closeButtonIn = Switch(pinCloseBtnIn);//,INPUT, HIGH);
 
 void setup() {
-  initPins();
-  allStop();
+  Serial.begin(9600);
+  Serial.println("setup");
+
+  initInputPins();
+  initOutputPins();
+  sendStop();
 }
 
 void loop() {
-  checkButtons();
+  ms = millis();
+
+  pollInput();
   updateMode();
-  delay(loopInterval);
+  sendOutput();
+
+  //delay(loopInterval);
 }
 
-//---
 
-void initPins() {
-  pinMode(openBtnOut, OUTPUT);
-  pinMode(closeBtnOut, OUTPUT);
-  pinMode(pBrakeOut, OUTPUT);
+/** Setup Functions **/
 
-  pinMode(openBtnIn, INPUT);
-  pinMode(closeBtnIn, INPUT);
+void initInputPins() {
+//  pinMode(pinOpenBtnIn, INPUT);
+//  pinMode(pinCloseBtnIn, INPUT);
 }
 
-void checkButtons() {
-  if(digitalRead(openBtnIn) == HIGH) {
-    handleOpenButtonPressed();
-  } else if(digitalRead(closeBtnIn) == HIGH) {
-    handleCloseButtonPressed();
-  } else {
-    handleNoButtonPressed();
-  }
+void initOutputPins() {
+  pinMode(pinOpenBtnOut, OUTPUT);
+  pinMode(pinCloseBtnOut, OUTPUT);
+  pinMode(pinParkingBrakeOut, OUTPUT);
 }
 
-void handleOpenButtonPressed() {
-  if(btnReleased){
-    allStop();
-    ignoreBtn = true;
-  } else if(ignoreBtn == false) {
-    if(openBtnCount < 32000) {
-      openBtnCount += loopInterval;
-    }
-    closeBtnCount = 0;
-    digitalWrite(closeBtnOut, LOW);
-  }
+/** Input Functions **/
+
+void pollInput() {
+
+  openButtonIn.poll();
+  closeButtonIn.poll();
+
 }
 
-void handleCloseButtonPressed() {
-  if(btnReleased){
-    allStop();
-    ignoreBtn = true;
-  } else if(ignoreBtn == false) {
-    if(closeBtnCount < 32000) {
-      closeBtnCount += loopInterval;
-    }
-    openBtnCount = 0;
-    digitalWrite(openBtnOut, LOW);
-  }
-}
-
-void handleNoButtonPressed() {
-  if (openMode != modeAutoOpen && openMode != modeAutoClose) {
-    allStop();
-    ignoreBtn = false;
-  } else {
-    btnReleased = true;
-    ignoreBtn = false;
-  }
-}
+/** Mode Update Functions */
 
 void updateMode() {
-  switch(openMode) { ////0=no auto movement, 1=auto open, 2 = auto close, 3 = manual open, 4 = manual close
-    case modeAutoOpen: //currently auto-opening
-      movementCount -= loopInterval;
-      if(openBtnCount > shortPressInterval) { //if the interval is greater, transition to manual open mode
-        openMode = modeManualOpen;
-      } else { //continue with auto-open
-        if(movementCount <= 0 || closeBtnCount > 0) { //stop auto-open function
-          allStop();
-        } else { //continue with auto-open function
-          //continue with current pin state
-        }
-      }
-      break;
 
-    case modeAutoClose: //currently auto-closing
-      movementCount -= loopInterval;
-      if(closeBtnCount > shortPressInterval) { //if the interval is greater, transition to manual close mode
-        openMode = modeManualClose;
-      } else { //continue with auto-close
-        if(movementCount <= 0 || openBtnCount > 0) { //stop auto-open function
-          allStop();
-        } else { //continue with auto-close function
-          //continue with current pin state
-        }
-      }
-      break;
+  switch (currentMode) {
 
-    case modeManualOpen: //currently manually opening
-      movementCount = 0;
-      if(openBtnCount > 0) {
-        //continue with current pin state
-      } else {
-        allStop();
-      }
+    case modeWait:
+      updateModeWait();
       break;
+    case modeAutoOpen:
+      updateModeAutoOpen();
+      break;
+    case modeAutoClose:
+      updateModeAutoClose();
+      break;
+    case modeManualOpen:
+      updateModeManualOpen();
+      break;
+    case modeManualClose:
+      updateModeManualClose();
+      break;
+    case modeHalt:
+      updateModeHalt();
+      break;
+  }
 
-    case modeManualClose: //currently manually closing
-      movementCount = 0;
-      if(closeBtnCount > 0) {
-        //continue with current pin state
-      } else {
-        allStop();
-      }
-      break;
+}
 
-    default: //no movement currently
-      if(openBtnCount > 0) {
-        startAutoOpen();
-      } else if(closeBtnCount > 0) {
-        startAutoClose();
-      } else {
-        allStop();
-      }
-      break;
+void updateModeWait() {
+  log("updateModeWait");
+  if (openButtonIn.on()) {
+    currentMode = modeAutoOpen;
+    motorStartTime = millis();
+  }
+  if (closeButtonIn.on()) {
+    currentMode = modeAutoClose;
+    motorStartTime = millis();
   }
 }
 
-void startAutoOpen() {
-  openMode = modeManualOpen;
-  movementCount = openTime;
-  digitalWrite(pBrakeOut, HIGH);
-  digitalWrite(closeBtnOut, LOW);
-  digitalWrite(openBtnOut, HIGH);
+void updateModeAutoOpen() {
+  log("updateModeAutoOpen");
+  //halt on button push
+  if (openButtonIn.pushed() || closeButtonIn.on()) {
+    currentMode = modeHalt;
+    return;
+  }
+  //wait on timer
+  if (ms - motorStartTime > openTime) {
+    currentMode = modeWait;
+    return;
+  }
+  //switch to manual mode on long press
+  if (openButtonIn.longPress()) {
+    currentMode = modeManualOpen;
+    return;
+  }
 }
 
-void startAutoClose() {
-  openMode = modeManualClose;
-  movementCount = closeTime;
-  digitalWrite(pBrakeOut, HIGH);
-  digitalWrite(openBtnOut, LOW);
-  digitalWrite(closeBtnOut, HIGH);
+void updateModeAutoClose() {
+  log("updateModeAutoClose");
+  //halt on button push
+  if (openButtonIn.on() || closeButtonIn.pushed()) {
+    currentMode = modeHalt;
+  }
+  //wait on timer
+  if (ms - motorStartTime > closeTime) {
+    currentMode = modeWait;
+    return;
+  }
+  //switch to manual mode on long press
+  if (closeButtonIn.longPress()) {
+    currentMode = modeManualClose;
+    return;
+  }
 }
 
-void allStop() {
-  digitalWrite(openBtnOut, LOW);
-  digitalWrite(closeBtnOut, LOW);
-  digitalWrite(pBrakeOut, LOW);
-  openMode = modeNull;
-  movementCount = 0;
-  openBtnCount = 0;
-  closeBtnCount = 0;
-  btnReleased = false;
+void updateModeManualOpen() {
+  log("updateModeManualOpen");
+  //wait on button release
+  if (!openButtonIn.on()) {
+    currentMode = modeWait;
+  }
 }
 
+void updateModeManualClose() {
+  log("updateModeManualClose");
+  //wait on button release
+  if (!closeButtonIn.on()) {
+    currentMode = modeWait;
+  }
+}
+
+void updateModeHalt() {
+  log("updateModeHalt");
+  //only release halt when both buttons are released
+  if (!closeButtonIn.on() && !openButtonIn.on()) {
+    currentMode = modeWait;
+  }
+}
+
+/** Output Functions */
+
+void sendOutput() {
+  if (currentMode != previousMode) {
+    log("mode changed");
+    Serial.println(currentMode);
+
+    if (currentMode == modeAutoOpen || currentMode == modeManualOpen) {
+      sendOpen();
+    } else if (currentMode == modeAutoClose || currentMode == modeManualClose) {
+      sendClose();
+    } else {
+      sendStop();
+    }
+  }
+
+  previousMode = currentMode;
+}
+
+void sendOpen() {
+  log("send : open");
+  digitalWrite(pinOpenBtnOut, HIGH);
+  digitalWrite(pinCloseBtnOut, LOW);
+  digitalWrite(pinParkingBrakeOut, HIGH);
+}
+
+void sendClose() {
+  log("send : close");
+  digitalWrite(pinOpenBtnOut, LOW);
+  digitalWrite(pinCloseBtnOut, HIGH);
+  digitalWrite(pinParkingBrakeOut, HIGH);
+}
+
+
+void sendStop() {
+  log("send : stop!");
+  digitalWrite(pinOpenBtnOut, LOW);
+  digitalWrite(pinCloseBtnOut, LOW);
+  digitalWrite(pinParkingBrakeOut, LOW);
+}
+
+void log(String log) {
+  if (log != lastLog) {
+    Serial.println(log);
+    lastLog = log;
+  }
+}
